7e5b00818779d08d2cbd47e2f869cedb13427e28
Carden Pool [CRPL] <86101039+CardenPool@users.noreply.github.com>
2023-08-11 18:00:02 +0200

    Update getstats.sh

--- getstats.sh
+++ getstats.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl https://js.cexplorer.io/api-static/pool/pool1e3y7u0rtq2zskc7ansvtrd4p3vtv9hglyztzcvsvlk22w9t40v0.json 2>/dev/null \
 | jq '.data' | jq 'del(.stats, .url , .img, .updated, .handles, .pool_id, .name, .pool_id_hash)' \
 | tr -d \"{},: \
